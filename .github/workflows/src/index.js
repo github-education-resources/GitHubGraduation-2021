be0a21d43fe70e9975745bbdc64bb1940d791f99
Adam Rensel <renz45@github.com>
2021-05-03 13:39:04 -0400

    GH_SECRET

--- .github/workflows/src/index.js
+++ .github/workflows/src/index.js
@@ -37,7 +37,7 @@ if (process.env.GITHUB_EVENT_NAME === "pull_request") {
 }
 
 // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
-const octokit = new Octokit({ auth: process.env.GITHUB_SECRET });
+const octokit = new Octokit({ auth: process.env.GH_SECRET });
 
 // Compare: https://docs.github.com/en/rest/reference/users#get-the-authenticated-user
 // const {
