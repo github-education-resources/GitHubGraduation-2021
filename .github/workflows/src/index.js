5d98022d3367753678b295c24bc8953fc19ca927
Adam Rensel <renz45@github.com>
2021-05-07 11:25:02 -0400

    logging

--- .github/workflows/src/index.js
+++ .github/workflows/src/index.js
@@ -37,56 +37,58 @@ const educationWeb = require('./app/education-web.js');
 
 const userData = actionEvent.pullRequest?.base.user;
 
-console.log(userData);
+console.log('EVENT USER: \n\n' + JSON.stringify(userData));
 
-(async ()=>{
-  const results = await Promise.all([
-    octokit.fetchPr(actionEvent.pullRequest?.number),
-    airtable.userParticipated2020(pull.author.name),
-    educationWeb.hasPack(),
-    airtable.fetch2021Graduate(pull.author.name)
-  ])
+console.log('EVENT: \n\n' + JSON.stringify(actionEvent))
 
-  const pull = results[0]
-  const user2021 = results[3]
+// (async ()=>{
+//   const results = await Promise.all([
+//     octokit.fetchPr(actionEvent.pullRequest?.number),
+//     airtable.userParticipated2020(pull.author.name),
+//     educationWeb.hasPack(),
+//     airtable.fetch2021Graduate(pull.author.name)
+//   ])
 
-  // checks
+//   const pull = results[0]
+//   const user2021 = results[3]
 
-  // graduated already in 2020?
-  const user2020 = results[1]
+//   // checks
 
-  // approved for the student/teacher development pack
-  const hasSdp = results[2]
+//   // graduated already in 2020?
+//   const user2020 = results[1]
 
-  // Has the user completed the shipping form? (address must exist for the form to be submitted)
-  const completedShippingForm = user2021.get("Address Line 1").length > 0
+//   // approved for the student/teacher development pack
+//   const hasSdp = results[2]
 
-  // pull.files TODO
-  // const pathIsCorrect = TODO
+//   // Has the user completed the shipping form? (address must exist for the form to be submitted)
+//   const completedShippingForm = user2021.get("Address Line 1").length > 0
 
-  // pull.body
-  // const markdownValid = TODO
+//   // pull.files TODO
+//   // const pathIsCorrect = TODO
 
-  // #################### TODO CACHE AIR TABLE SOMEHOW ########################
-  // * cache the entire base in a json file with actions
-  // * if the user checks come back negative, query the api directly to double check
-  // * if it comes back different, then trigger a cache rebuild
+//   // pull.body
+//   // const markdownValid = TODO
 
-  const userAgreesCoc = user2021.get("Code of Conduct").length > 0
+//   // #################### TODO CACHE AIR TABLE SOMEHOW ########################
+//   // * cache the entire base in a json file with actions
+//   // * if the user checks come back negative, query the api directly to double check
+//   // * if it comes back different, then trigger a cache rebuild
 
-  if(user2020) {
-    console.log("user already Participated in 2020")
-  } else if(!hasSdp) {
-    console.log("User has not applied for SDP")
-  } else if(!completedShippingForm) {
-    console.log("user has not completed the shipping form")
-  } else if(!pathIsCorrect) {
-    console.log("user file path is incorrect")
-  } else if(!markdownValid) {
-    console.log("markdown is invalid")
-  } else if(!userAgreesCoc) {
-    console.log("User has not agreed to COC")
-  } else {
-    console.log("Congrats you graduated!")
-  }
-})()
+//   const userAgreesCoc = user2021.get("Code of Conduct").length > 0
+
+//   if(user2020) {
+//     console.log("user already Participated in 2020")
+//   } else if(!hasSdp) {
+//     console.log("User has not applied for SDP")
+//   } else if(!completedShippingForm) {
+//     console.log("user has not completed the shipping form")
+//   } else if(!pathIsCorrect) {
+//     console.log("user file path is incorrect")
+//   } else if(!markdownValid) {
+//     console.log("markdown is invalid")
+//   } else if(!userAgreesCoc) {
+//     console.log("User has not agreed to COC")
+//   } else {
+//     console.log("Congrats you graduated!")
+//   }
+// })()
