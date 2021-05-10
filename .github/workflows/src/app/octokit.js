90a151ac9a86e9c30f74f6eec9653c0f4771c9c2
Adam Rensel <renz45@github.com>
2021-05-10 14:59:37 -0400

    Get reviews and create reviews

--- .github/workflows/src/app/octokit.js
+++ .github/workflows/src/app/octokit.js
@@ -18,7 +18,7 @@ class Octo {
     });
 
     return data
-   }
+  }
 
   async fetchPr(pr) {
     const { repository: { pullRequest } } = await this.octokit.graphql(
@@ -31,14 +31,26 @@ class Octo {
               }
               bodyText
               closed
-              comments(first: 100) {
-                edges {
+              reviews(first:100) {
+                edges{
                   node {
+                    body
+                    state
+                    url
                     author {
                       login
                     }
-                    body
-                    url
+
+                    comments(first: 100) {
+                      edges {
+                        node {
+                          body
+                          author {
+                            login
+                          }
+                        }
+                      }
+                    }
                   }
                 }
               }
@@ -63,6 +75,33 @@ class Octo {
       }
     )
     return pullRequest
+  }
+
+  // comments are an array of objects:
+
+  // path (string) Required. The relative path to the file that necessitates a review comment.
+  // position (integer) The position in the diff where you want to add a review comment.
+  //          Note this value is not the same as the line number in the file. For help finding
+  //          the position value, read the note below.
+  // body (string) Required. Text of the review comment.
+  // line (integer)
+  // side (string)
+  // start_line (integer)
+  // start_side (string)
+
+  async createReview(content, event="COMMENT", comments=[]) {
+    const { data }  = await this.octokit.rest.pulls.createReview({
+      accept: "application/vnd.github.v3+json",
+      pull_number: actionEvent.pullNumber,
+      commit_id: actionEvent.pull.head.sha,
+      owner: actionEvent.pullRepoOwner,
+      repo: actionEvent.pullRepo.name,
+      body: content,
+      comments: comments,
+      event: event
+    });
+
+    return data
   }
 }
 
