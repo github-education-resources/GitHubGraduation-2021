b4807fd57acf2308f4cfa36b2876e0b2492c021c
Adam Rensel <renz45@github.com>
2021-05-11 00:23:32 -0400

    Absolute paths go

--- .github/workflows/src/app/fetch-data.js
+++ .github/workflows/src/app/fetch-data.js
@@ -6,7 +6,7 @@ const fs = require('fs')
   const grad2021 = await airtable.fetchAll2021()
   const content = JSON.stringify({grad2020, grad2021})
 
-  fs.writeFile('./app/data/airtable.json', content, err => {
+  fs.writeFile('/home/runner/work/GitHubGraduation-2021/GitHubGraduation-2021/.github/workflows/src/app/data/airtable.json', content, err => {
     if (err) {
       console.error(err)
       process.exit(1)
